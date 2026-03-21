46339dd7dfce0c19b90b62fe0f4129d28f5878db
Francine-G <francinegabrielle.luces@wvsu.edu.ph>
2026-03-21 13:09:57 +0800

    downloaded postrgres

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,7 +4,17 @@ import { PrismaClient } from "../generated/prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export { prisma };
+const createPrismaClient = () => {
+  const adapter = new PrismaPg({ connectionString });
+  return new PrismaClient({ adapter });
+};
+
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
